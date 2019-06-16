96a5294cc373a22d273c3d2af31ee3801d93de34
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-16 15:51:25 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,20 +13,20 @@ end
 # map([1, 2, 3, -9]){|n| n * -1}
 # map(dune){|n| n}
 
-def reduce(source_array, start=true)
-  if start == true
-    starting = 0 #source_array[0]
-  else starting = start
-  end
-
-  n = 0
-  while n < source_array.length do
-    starting = yield(starting, source_array[n])
-    puts "starting is #{starting}"
-    n += 1
-  end
-  starting
-end
+# def reduce(source_array, start=true)
+#   if start == true
+#     starting = 0 #source_array[0]
+#   else starting = start
+#   end
+#
+#   n = 0
+#   while n < source_array.length do
+#     starting = yield(starting, source_array[n])
+#     puts "starting is #{starting}"
+#     n += 1
+#   end
+#   starting
+# end
 
 
 
