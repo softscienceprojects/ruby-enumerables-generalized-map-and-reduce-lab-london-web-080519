2ea851fff7dea0a37b66c069746c2c814f4f637b
erin johnson <erin.nicole.johnson@gmail.com>
2019-06-16 16:48:59 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,32 @@
 # Your Code Here
+
+def map(arr)
+  newArr = []
+  i = 0
+  while i < arr.length
+    newArr.push(yield arr[i])
+    i+= 1
+  end
+  newArr
+end
+
+# map([1, 2, 3, -9]){|n| n * -1}
+# map(dune){|n| n}
+
+
+def reduce(source_array, start=nil)
+  if start == nil
+    starting = source_array[0]
+    n = 1
+  else starting = start
+    n=0
+  end
+
+  while n < source_array.length do
+    starting = yield(starting, source_array[n])
+    puts "starting is #{starting}"
+    # reduce(source_array){|memo, n| memo + n}
+    n += 1
+  end
+  starting
+end
